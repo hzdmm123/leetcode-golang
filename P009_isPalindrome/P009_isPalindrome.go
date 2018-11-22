d8acf95448c8a2f9c1df6e3b26977991244ca39d
hzdmm123 <hzdmm123@163.com>
2018-11-22 10:05:27 +0800

    add solution 2 for p009

--- P009_isPalindrome/P009_isPalindrome.go
+++ P009_isPalindrome/P009_isPalindrome.go
@@ -37,3 +37,15 @@ func isPalindrome(x int) bool {
 	}
 	return true
 }
+
+func isPalindrome_II(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	revertNum := 0
+	for x > revertNum {
+		revertNum = revertNum*10 + x%10
+		x = x / 10
+	}
+	return x == revertNum || x == revertNum/10
+}
